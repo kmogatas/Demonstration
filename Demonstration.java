51a798cfb90e3a2575b861eb2020449e473bd740
kmogatas <kaleighrossmogatas@gmail.com>
2022-05-01 18:46:48 -0700

    Added Test file.

--- Demonstration.java
+++ Demonstration.java
@@ -1,6 +1,16 @@
 public class Demonstration{
 
-// Code
+    public static int basicallyMultiplication(int num, int num2){
 
+        int total = 0;
+
+        for(int i = 0; i < 5; i++){
+            total = (num + num2);
+        }
+
+        return total;
+    }
+
+    // There is a bug in this code.
 
 }
---
+++ DemonstrationTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class DemonstrationTest {
+    
+    @Test 
+    public void basicallyMultiplication(){
+        assertEquals(15, Demonstration.basicallyMultiplication(1, 2));
+    }
+    //This test will fail.
+}
